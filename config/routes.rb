bf18ea3cd1ad0add7ce71e41e770411362008a32
Alvaro <alvarodltp@gmail.com>
2018-03-16 12:54:36 -0500

    Add workout forms/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :workouts
+
   root 'workouts#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'workouts/new'
 end
